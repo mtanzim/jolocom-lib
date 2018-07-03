401dccf2200934cfd8b8ce19dd1b90179db52d5d
Kasia Szawan <kat.szwn@gmail.com>
2018-07-03 17:04:36 +0200

    Initial setup for sphinx-js with typescript

---
+++ docs/_build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{current:0,index:0,modul:0,page:0,search:0,thi:0,work:0},titles:["Welcome to Jolocom-Lib\u2019s documentation!"],titleterms:{document:0,indic:0,jolocom:0,lib:0,tabl:0,welcom:0}})
--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinx_js'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -59,6 +60,9 @@ master_doc = 'index'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = None
+
+js_language = 'typescript'
+js_source_path = './ts'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
